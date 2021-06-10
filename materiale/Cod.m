59ddd808692be8e123174b20c4ee28ecd7aa5858
Albert Luta <luta.albert100@gmail.com>
2021-06-10 16:38:16 +0300

    Init

---
+++ materiale/Cod.m
@@ -0,0 +1,15 @@
+function [ out ] = Cod( varinput )
+% acesta este un comentariu
+% param intrare varinput
+% param iesire out
+
+rez = 0;
+n=10;
+for i=1:n                                                                                                                                              
+    for j=i+1:n-1
+    if abs(-2)<abs(-3)
+       rez = rez +1;
+    end
+    end
+end
+end
