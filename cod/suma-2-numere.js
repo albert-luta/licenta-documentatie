f0099d694eee7126314ee84a35918abcf466c956
Albert Luta <luta.albert100@gmail.com>
2021-06-17 21:42:37 +0300

    Added the first iteration of the 'Tehnologii folosite' - frontend

---
+++ cod/suma-2-numere.js
@@ -0,0 +1,6 @@
+function sumaJs(a, b) {
+  return a + b;
+}
+
+sumaJs(1, 5); // 6
+sumaJs(1, "5"); // "15"
---
+++ cod/suma-2-numere.ts
@@ -0,0 +1,7 @@
+function sumaTs(a: number, b: number): number {
+	return a + b;
+}
+
+sumaTs(1, 5); // 6
+sumaTs(1, "5"); // Eroare: Argumentul 2 de tip 'string' nu poate fi atribuit unui parametru de tip 'number'
+
